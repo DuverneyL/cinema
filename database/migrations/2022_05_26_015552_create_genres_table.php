fa02854e5cb8e49ee21ecd4db668fadb41357c81
duverney <duverney.loaiza@gmail.com>
2022-05-25 22:59:46 -0400

    Creacion modelo Movie Genre con claves foranea y migracion

---
+++ database/migrations/2022_05_26_015552_create_genres_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('genre');
+            $table->timestamps();
+        });
+
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('cast');
+            $table->string('direction');
+            $table->string('duration');
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('genres');
+    }
+}
